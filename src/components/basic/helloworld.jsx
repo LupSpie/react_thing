e02ff5734a2730c12b7b10fbb7b9f33bc447ab87
lupspie <lupspie@outlook.com>
2021-03-15 10:53:36 -0300

    baby steps

---
+++ src/components/basic/helloworld.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => {
+  const info = 'Primeiro componente JSX'
+
+  return(
+    <>
+      <h2>Componente WorldHello</h2>
+      <p>{ info }</p>
+    </>
+  )
+}
